28fbda7c376044d5140f4b2fc629312383ede0ed
Fransiskus Agapa <agapaf@spu.edu>
2025-01-23 00:06:40 -0800

    added TODO txt file

--- main.go
+++ main.go
@@ -7,28 +7,6 @@ import (
 	// allow 'time'
 )
 
-// TODO:
-// # UX
-// # 1. Provide hint: how 'close' their guess to the actual number
-// # 2. Provide Guess History: a way to keep track of previous guess & display it
-// # 3. Set timer/timeout: countdown/limit the number of guess
-// # Interactive Feedback
-// # 4. Progressive Feedback: Instead of just saying "Try lower" or "Try higher," include more engaging responses, like "You're getting warmer!" or "You're freezing cold!"
-// # 5. Sound Effects: Use sound effects to provide feedback on user actions, such as a "ding" when they guess correctly or a "buzz" for incorrect guesses.
-// # Personalization
-// # 6. Set player's name
-// # 7. Leaderboard: Implement a scoring system where players earn points based on how quickly they guess the number, and display a leaderboard of top scores.
-// # Error Handling and Input Validation
-// # 8. Provide friendly error messages for invalid inputs.
-// # 9. Retry Option: After the game ends, give the player the option to play again without restarting the entire program.
-// # Story / Theme
-// # 10. Themed Game: Add a narrative or theme to the game, such as guessing a number to unlock a treasure chest or as part of a larger adventure. This can increase engagement by giving the game context and meaning.
-// # 11. Progressive Challenges: Introduce new challenges or mini-games as the user progresses through levels or wins multiple rounds, such as more difficult numbers to guess or added obstacles.
-// # Multiplayer option
-// # 12. Competitive Mode: Allow multiple players to take turns guessing the number and see who can guess it in the fewest attempts.
-// # 13. Tracking Statistic
-// # 14. Guessing Stats: Track statistics such as the average number of guesses per game, the longest streak of correct guesses, and the time taken to guess the number. Display these stats at the end of the game to encourage improvement.
-
 func main() {
 	// prompt and validate user guess limit
 	var numberToGuess int = promptGuessLimit()
